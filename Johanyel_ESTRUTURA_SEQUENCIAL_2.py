e118bef6a59e1f341300919576478104ad4055d5
johanyel2008 <johanyel_maita@estudante.sesisenai.org.br>
2025-10-07 19:52:05 -0300

    Create Johanyel_ESTRUTURA_SEQUENCIAL_2.py

---
+++ Johanyel_ESTRUTURA_SEQUENCIAL_2.py
@@ -0,0 +1,3 @@
+#Faça um programa que peça um número e então mostre a mensagem "O número informado foi [número]":
+numero1=int(input("Informe seu número: "))
+print("O número informado foi",numero1)
