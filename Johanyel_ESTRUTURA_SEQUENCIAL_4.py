e7aeaa3316352d190f27b6d943a9e684e7405b3c
johanyel2008 <johanyel_maita@estudante.sesisenai.org.br>
2025-10-07 20:56:32 -0300

    Create Johanyel_ESTRUTURA_SEQUENCIAL_4.py

---
+++ Johanyel_ESTRUTURA_SEQUENCIAL_4.py
@@ -0,0 +1,10 @@
+#Faça um programa que peça as 4 notas bimestrais e mostre a média.
+nota1=float(input("Informe uma nota : "))
+nota2=float(input("Informe uma nota : "))
+nota3=float(input("Informe uma nota : "))
+nota4=float(input("Informe uma nota : "))
+media=(nota1+nota2+nota3+nota4)/4
+print(media)
+
+
+
