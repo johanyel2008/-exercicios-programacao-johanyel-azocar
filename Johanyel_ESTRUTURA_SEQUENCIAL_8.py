98a1ef36e579fdeba8a081af9c1cab5909795b3b
johanyel2008 <johanyel_maita@estudante.sesisenai.org.br>
2025-10-07 21:17:56 -0300

    jo

---
+++ Johanyel_ESTRUTURA_SEQUENCIAL_8.py
@@ -0,0 +1,6 @@
+#Faça um programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês.
+valor_hora=float(input('Quanto você ganha por hora: '))
+horas_mes=float(input('Quantas horas trbalhou esse mês: '))
+salário = valor_hora*horas_mes
+print("O seu Salario é",salário )
+
